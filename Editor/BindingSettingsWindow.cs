b94771e9148fadc04e730d738bac14d458cea2e7
饭炒蛋 <zhouzelong.cn@gmail.com>
2024-05-07 21:37:16 +0800

    设置面板中添加注入模式开关

--- Editor/BindingSettingsWindow.cs
+++ Editor/BindingSettingsWindow.cs
@@ -1,10 +1,13 @@
-using UIBinding;
+using System;
+using System.Collections.Generic;
 using UnityEditor;
 
 namespace UIBindingEditor
 {
     public class BindingSettingsWindow : EditorWindow
     {
+        private const string DISABLE_UIBINDING_INJECT = "DISABLE_UIBINDING_INJECT";
+
         private Editor editor;
 
         [MenuItem("Tools/UIBinding/Settings")]
@@ -20,7 +23,44 @@ namespace UIBindingEditor
             if (editor != null)
             {
                 editor.OnInspectorGUI();
+                DrawInjectSwitch();
             }
+        }
+
+        private void DrawInjectSwitch()
+        {
+            var oldDisableInject = IsDisableInject();
+            var newDisableInject = EditorGUILayout.Toggle($"Disable inject", oldDisableInject);
+            if (newDisableInject != oldDisableInject)
+            {
+                SetDisableInject(newDisableInject);
+            }
+        }
+
+        private void SetDisableInject(bool disable)
+        {
+            BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            var defineArr = defines.Split(';');
+            if (disable && Array.IndexOf(defineArr, DISABLE_UIBINDING_INJECT) < 0)
+            {
+                var defineList = new List<string>(defineArr);
+                defineList.Add(DISABLE_UIBINDING_INJECT);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(';', defineList));
+            }
+            else if (!disable && Array.IndexOf(defineArr, DISABLE_UIBINDING_INJECT) >= 0)
+            {
+                var defineList = new List<string>(defineArr);
+                defineList.Remove(DISABLE_UIBINDING_INJECT);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(';', defineList));
+            }
+        }
+
+        private bool IsDisableInject()
+        {
+            BuildTargetGroup buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            return defines.Contains(DISABLE_UIBINDING_INJECT, StringComparison.Ordinal);
         }
     }
 }
